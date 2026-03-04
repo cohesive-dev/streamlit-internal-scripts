eef5cfac4278c705165b7704c606491460711f4a
cohesiveadmin <admin@cohesiveapp.com>
2026-03-04 11:52:38 -0500

    Remove duplicated script

--- app.py
+++ app.py
@@ -68,10 +68,6 @@ nav = st.navigation(
             title="Edit Campaign Template",
         ),
         st.Page(
-            "pages/va/edit_campaign.py",
-            title="Edit Campaign Template",
-        ),
-        st.Page(
             "pages/va/revert_campaign_template.py",
             title="Revert Campaign Template",
         ),
